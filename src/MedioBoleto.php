17e8abc40c66c6300a0c3693720c175f96fe3253
Daniel Lezcano <daniel.lezcano727@gmail.com>
2018-08-21 09:11:03 -0300

    Cambiada variable por precio en MedioBoleto.php

--- src/MedioBoleto.php
+++ src/MedioBoleto.php
@@ -5,13 +5,6 @@ namespace TrabajoTarjeta;
 class MedioBoleto extends Tarjeta {
     function __construct(){
         parent::__construct();
-    }
-
-    public function pagarPasaje(){
-        if($this->saldo >= (-7.40)){
-            $this->saldo -=7.40;
-            return true;
-          }
-          return false;
+        $this->precio /= 2;
     }
 }
