90ee90e838f280ed8c9b4c9937fbb00930d98e57
Daniel Lezcano <daniel.lezcano727@gmail.com>
2018-09-04 09:23:08 -0300

    Eliminacion codigo innecesario (llamado a los constructores padre)

--- src/FranquiciaCompleta.php
+++ src/FranquiciaCompleta.php
@@ -3,9 +3,6 @@
 namespace TrabajoTarjeta;
 
 class FranquiciaCompleta extends Tarjeta {
-    function __construct(){
-        parent::__construct();
-    }
 
     public function pagarPasaje(){
         return true;
--- src/MedioBoleto.php
+++ src/MedioBoleto.php
@@ -4,7 +4,6 @@ namespace TrabajoTarjeta;
 
 class MedioBoleto extends Tarjeta {
     function __construct(){
-        parent::__construct();
         $this->precio /= 2;
     }
 }
