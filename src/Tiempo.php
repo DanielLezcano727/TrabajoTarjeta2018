da23fd5b01a00bd426a187a94daa02a16b57a6dc
Micaela Siles <micaela.b.siles@gmail.com>
2018-09-09 23:42:14 -0300

    Añadida una clase para la simulacion del tiempo (TiempoFalso)

--- src/Tiempo.php
+++ src/Tiempo.php
@@ -7,3 +7,20 @@ class Tiempo implements TiempoInterface {
     }
     
 }
+
+class TiempoFalso implements TiempoInterface {
+
+    protected $tiempo;
+
+    public function __construct($inicio = 0) {
+        $this->tiempo = $inicio;
+    }
+    
+    public function avanzar($segundos){
+        $this->tiempo += $segundos;
+    }
+
+    public function time(){
+        return $this->tiempo;
+    }
+}
