f722fdf8c22a55b4ff2897a6002f03915ea99ac2
Micaela Siles <micaela.b.siles@gmail.com>
2018-09-10 23:55:43 -0300

    Limitacion de tiempo y de cantidad de viajes añadidas para los distintos tipos de media franquicia

--- src/MedioBoleto.php
+++ src/MedioBoleto.php
@@ -3,8 +3,60 @@
 namespace TrabajoTarjeta;
 
 class MedioBoleto extends Tarjeta {
-    function __construct(){
+    protected $tipo;
+    protected $tiempo;
+    protected $tiempoAux;
+    protected $cantPorDia;
+
+    public function __construct($tipo = 1, TiempoInterface $tiempo){
         parent::__construct();
         $this->precio /= 2;
+        switch($tipo){
+        case 0:
+            $this->tipo = "Universitario";
+            break;
+        case 1:
+        default:
+            $this->tipo = "Secundario";
+            break;
+        }
+        $this->tiempo = $tiempo;
+        $this->tiempoAux = 0;
+        $this->cantPorDia = 0;
+    }
+    
+    static protected $usos;
+
+    public function pagarPasaje(){
+
+        static $aumento = true;
+
+        if(($tiempo - $tiempoAux ) % 86400 == 0 && $tiempo != $tiempoAux){
+            $aumento = true;
+            $this->usos = 0;
+        }
+
+        if(($this->tipo == "Universitario" && $this->usos != 2) || $this->tipo == "Secundario"){
+            if($this->tiempoAux - $this->tiempo){
+                $x = parent::pagarPasaje();
+                $this->tiempoAux = $this->tiempo->time();
+                $this->cantPorDia++;
+            }else{
+                $this->precio *= 2;
+                $x = parent::pagarPasaje();
+                $this->precio /= 2;
+                
+            }
+            return $x;
+
+        }else{
+            
+            if($aumento){
+                $this->precio *= 2;
+                $aumento = false;
+            }
+
+        }
+    
     }
 }
