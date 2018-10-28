ac1582e4ca1adaae81483f072cad9e336a6464ec
Daniel Lezcano <daniel.lezcano727@gmail.com>
2018-10-27 23:27:27 -0300

    Disminucion de la complejidad del codigo del constructor de la clase Boleto.php

--- src/Boleto.php
+++ src/Boleto.php
@@ -14,10 +14,55 @@ class Boleto implements BoletoInterface {
     protected $id;
     protected $descripcion;
 
+
+
     public function __construct($colectivo, $tarjeta, TiempoInterface $tiempo) {
         $this->colectivo = $colectivo;
         $this->tarjeta = $tarjeta;
 
+        $this->establecerTipo($tarjeta);
+
+        $this->linea = $colectivo->linea();
+        
+        $this->total = $tarjeta->obtenerPrecio() + $tarjeta->obtenerPlusAbonados() * $tarjeta->obtenerPrecio();
+        
+        $this->saldo = $tarjeta->obtenerSaldo();
+        
+        $this->id = $tarjeta->obtenerID();
+        
+        $this->fecha = date("d/m/Y H:i:s",$tiempo->time());
+        
+        $this->descripcion = $this->establecerDescripcion($tarjeta->obtenerCantPlus(), $tarjeta->obtenerPlusAbonados(), $tarjeta);
+
+        $tarjeta->reestablecerPrecio();
+    }
+
+    private function establecerDescripcion($cantPlus, $plusAbonados, $tarjeta){
+        
+        switch($cantPlus){
+        case 1:
+            return "$0 Viaje Plus";
+        case 2:
+            return "$0 Ultimo Plus";
+        }
+        
+        $abona = "";
+        
+        switch($plusAbonados){
+        case 1:
+            $abona = " Abona un Viaje Plus";
+            break;
+        case 2:
+            $abona = " Abona dos Viajes Plus";
+            break;
+        }
+        
+        $tarjeta->reestablecerPlus();
+
+        return "$" . ($tarjeta->obtenerPrecio() + 14.8 * $plusAbonados) . $abona;
+    }
+
+    private function establecerTipo($tarjeta){
         switch($tarjeta->obtenerPrecio()){
         case 14.80:
             $this->tipo = "Normal";
@@ -29,56 +74,21 @@ class Boleto implements BoletoInterface {
             $this->tipo = "Franquicia completa";
             break;
         default:
-            switch($tarjeta->obtenerPrecio()){
-            case (14.8/3):
-                $this->tipo = "Trasbordo";
-                break;
-            case (7.4/3):
-                $this->tipo = "Medio Trasbordo";
-                break;
-            }
+            $this->tipo = $this->tipoTrasbordo($tarjeta->obtenerPrecio());
             $tarjeta->reestablecerPrecio();
             break;
         }
-
-        
-        $this->linea = $colectivo->linea();
-        
-        $this->total = $tarjeta->obtenerPrecio() + $tarjeta->obtenerPlusAbonados() * $tarjeta->obtenerPrecio();
-        
-        $this->saldo = $tarjeta->obtenerSaldo();
-        
-        $this->id = $tarjeta->obtenerID();
-        
-        $this->fecha = date("d/m/Y H:i:s",$tiempo->time());
-        
-        switch($tarjeta->obtenerCantPlus() ){
-        case 1:
-            $this->descripcion = "$0 Viaje Plus";
-            break;
-        case 2:
-            $this->descripcion = "$0 Ultimo Plus";
-            break;
-        case 0:
-            switch($tarjeta->obtenerPlusAbonados()){
-            case 1:
-                $this->descripcion = "$" . ($tarjeta->obtenerPrecio() + 14.8) . " Abona un Viaje Plus";
-                $tarjeta->reestablecerPlus();
-                break;
-            case 2:
-                $this->descripcion = "$" . ($tarjeta->obtenerPrecio() + 14.8 * 2) . " Abona dos Viajes Plus";
-                $tarjeta->reestablecerPlus();
-                break;
-            case 0:
-                $this->descripcion = "$" . $tarjeta->obtenerPrecio();
-                break;
-            }
-            break;
-        }
-
-        $tarjeta->reestablecerPrecio();
     }
-    
+
+    private function tipoTrasbordo($precio){
+        switch($precio){
+        case (14.8/3):
+            return "Trasbordo";
+        case (7.4/3):
+            return "Medio Trasbordo";
+        }
+    }
+
     /**
      * Devuelve el tarjeta->obtenerPrecio() del boleto.
      *
